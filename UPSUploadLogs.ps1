3297cbdc125bc0697d5e937b6d51f7d591040dc0
srgkiryanov84 <srgkiryanov@yandex.ru>
2026-03-15 23:47:23 +0300

    added posh script and readme for this script

---
+++ UPSUploadLogs.ps1
@@ -0,0 +1,20 @@
+﻿# ================================
+# = проверка работы branch в git =
+# ================================
+#$UPSis = @("123.123.12.34", "234.567.12.34") # UPS IP-adress
+$UPSis = @("172.26.216.21", "172.26.216.22") # UPS IP-adress
+$user = 'UPSLogin'
+$password = 'UPSPassword'
+$UploadFolder = "$($PSScriptRoot)\"
+$FileforUpload1 = "data.txt"
+$FileforUpload2 = "event.txt"
+foreach ($UPS in $UPSis){
+    $connection = New-Object System.Net.WebClient
+    $connection.Credentials = New-Object System.Net.NetworkCredential("$user", "$password")
+    $DataTime = Get-Date -Format "dd/MM/yyyy HH-mm"
+    if ($UPS -eq "172.26.216.21"){$logfile = "_Your_1UPSName_"; $logfolder = "Your_1UPSName"}
+    else {$logfile = "_Your_2UPSName_"; $logfolder = "Your_2UPSName"}
+    $logfilename = $logfile + $DataTime
+    $connection.DownloadFile("ftp://$UPS/$FileforUpload1", "$UploadFolder\$logfolder\$("data" + $logfilename).txt")
+    $connection.DownloadFile("ftp://$UPS/$FileforUpload2", "$UploadFolder\$logfolder\$("event" + $logfilename).txt")
+}
